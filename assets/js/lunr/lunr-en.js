649d8c147dba41c6fb0d68288563869ba6d76acb
Joonhoo <mnc5@naver.com>
2023-02-24 02:51:40 +0900

    검색창과 검색 목록 디자인 변경

--- assets/js/lunr/lunr-en.js
+++ assets/js/lunr/lunr-en.js
@@ -46,15 +46,16 @@ $(document).ready(function() {
         var searchitem =
           '<div class="list__item">'+
             '<article class="archive__item" itemscope itemtype="https://schema.org/CreativeWork">'+
-              '<h2 class="archive__item-title" itemprop="headline">'+
-                '<a href="'+store[ref].url+'" rel="permalink">'+store[ref].title+'</a>'+
-              '</h2>'+
               '<div class="archive__item-teaser">'+
                 '<img src="'+store[ref].teaser+'" alt="">'+
               '</div>'+
+              '<h2 class="archive__item-title" itemprop="headline">'+
+                '<a href="'+store[ref].url+'" rel="permalink">'+store[ref].title+'</a>'+
+              '</h2>'+
               '<p class="archive__item-excerpt" itemprop="description">'+store[ref].excerpt.split(" ").splice(0,20).join(" ")+'...</p>'+
             '</article>'+
-          '</div>';
+          '</div>' +
+          '<hr class="archive__item_hr">';
       }
       else{
     	  var searchitem =
