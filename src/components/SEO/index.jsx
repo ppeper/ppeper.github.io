9f8c2cb2922e5e8e38e3a9f30c8845d4f3a6a461
Joonhoo <joonhoo0123@gmail.com>
2025-03-16 21:54:46 +0900

    google site verification 등록

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -11,6 +11,7 @@ const SEO = ({ title, description, url }) => {
       <meta property="og:image" content={`${siteUrl}/og-image.png`} />
       {description && <meta name="description" content={description} />}
       {description && <meta property="og:description" content={description} />}
+      <meta name="google-site-verification" content="9Idw5Z0BgU2mz59GlJtS6QXTZ9qDOqI7tIFhI4rp_vQ" />
     </Helmet>
   )
 }
